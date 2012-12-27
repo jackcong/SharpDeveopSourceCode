fbc9883dc8231e05e5ec761743296b5049c3dd43
jack.cong <cong.lianjian@t2vsoft.com>
2012-12-27 14:18:51 +0800

    first commit

---
+++ src/AddIns/Misc/UsageDataCollector/UsageDataCollector.AddIn/AnalyticsMonitor.AppProperties.cs
@@ -0,0 +1,17 @@
+﻿// <file>
+//     <copyright see="prj:///doc/copyright.txt"/>
+//     <license see="prj:///doc/license.txt"/>
+//     <author name="Daniel Grunwald"/>
+//     <version>$Revision$</version>
+// </file>
+
+using System;
+
+namespace ICSharpCode.UsageDataCollector
+{
+	partial class AnalyticsMonitor
+	{
+		const string BranchName = "4.2";
+		const string CommitHash = "9345aa7cc9dad00f873e8ae341943b7c2a080b7e";
+	}
+}
